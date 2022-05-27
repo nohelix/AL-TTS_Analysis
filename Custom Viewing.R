3cf21e511b4b5f2b1d7b093f5fd4fabd2cefd028
Noelle James <github@nohelix.com>
2022-05-27 15:00:12 -0500

    Added calculation of days run & total number of trials for each phase and total by rat
    Also added the complete package loading.

--- Custom Viewing.R
+++ Custom Viewing.R
@@ -1,14 +1,41 @@
 
 # Notes -------------------------------------------------------------------
 
+# Package loading ---------------------------------------------------------
+
+# data loading/manipulation
+library(readxl); library(tidyverse); library(magrittr); library(dplyr); library(tidyr);
+
+# Analysis
+library(psych); library(psycho); library(lme4); library(lmerTest);
+
+# Data visualization
+library(ggplot2); library(forcats);
+
+# Exporting data
+library(writexl)
+
+# Phase Day numbers -------------------------------------------------------
+
+TTS_Data %>%
+  filter(Condition %in% c("Baseline", "Post HHL")) %>%
+  filter(!(ID %in% HL_not_done)) %>%
+  group_by(ID, Condition, Duration, Phase, Frequency, BG_Type, BG_Intensity) %>%
+  summarise(Days = n(), Trials = sum(Trials, na.rm = TRUE))
+
+TTS_Data %>%
+  filter(Condition %in% c("Baseline", "Post HHL")) %>%
+  filter(!(ID %in% HL_not_done)) %>%
+  group_by(ID, Condition, Duration, Frequency, Intensity, BG_Type, BG_Intensity) %>%
+  summarise(Days = n(), Trials = sum(Trials, na.rm = TRUE))
 
 
 # Threshold filtering -----------------------------------------------------
 
-TH %>%
-  filter(`Dur (ms)` != "100") %>%
-  filter(Condition %in% c("Baseline")) %>%
-  filter(!(ID %in% HL_not_done)) %>%
-  filter(BG_Intensity %in% c("NA", "50")) %>%
-  # write.table("clipboard", sep="\t", row.names=FALSE) %>%
-  View
+# TH %>%
+#   filter(`Dur (ms)` != "100") %>%
+#   filter(Condition %in% c("Baseline")) %>%
+#   filter(!(ID %in% HL_not_done)) %>%
+#   filter(BG_Intensity %in% c("NA", "50")) %>%
+#   # write.table("clipboard", sep="\t", row.names=FALSE) %>%
+#   View
