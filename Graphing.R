f6744803c9e710c0eaf39c5b0955a9dc434f34ad
Noelle James <github@nohelix.com>
2022-06-16 09:34:08 -0500

    Moved all graphs to graphing and cleaned up.

--- Graphing.R
+++ Graphing.R
@@ -192,3 +192,231 @@ ggsave("Rxn_Change.jpg",
        plot = last_plot(), # or an explicit ggplot object name
        path = ProjectFolder,
        width = 1200, height = 600, units = "px", dpi = 100)
+
+# Psychoacuostic Graph -----------------------------------------------------
+
+
+# Calculate standard error (SE) like standard deviation (SD)
+se <- function(x, ...) {sqrt(var(x, ...)/length(x))}
+
+n_fun <- function(x){
+  # print(x)
+  return(data.frame(y = mean(x), label = paste0("n = ",length(x))))
+}
+
+
+# Change in d' by BG ------------------------------------------------------
+# Overview (Change by Background)
+
+dprime %>%
+  filter(Condition == "dprime_change") %>%
+  left_join(Avg_TH, by = c("Type", "BG_Type", "BG_Intensity")) %>%
+  ggplot(aes(x = dB, y = dprime, color = Type, group = Type)) +
+  geom_hline(yintercept = 0, linetype = "dashed", size = 1.2) +
+  # Add vline for each average threshold
+  geom_vline(aes(xintercept = TH, color = Type), linetype = "longdash", size = 1, show.legend = FALSE) +
+  stat_summary(fun = mean,
+               fun.min = function(x) mean(x) - se(x),
+               fun.max = function(x) mean(x) + se(x),
+               geom = "errorbar", width = 1, position = position_dodge(1)) +
+  stat_summary(fun = mean, geom = "point", position = position_dodge(1), size = 3) +
+  stat_summary(fun = mean, geom = "line") +
+  # stat_summary(fun.data = n_fun, geom = "text", vjust = 5, show.legend = FALSE) +
+  scale_x_continuous(limits = c(-1, 91), breaks = c(10,30,50,70,90)) +
+  labs(x = "Intensity (dB)",
+       y = "Change in d' following HHL (+/- SE)",
+       color = "Go Frequency") +
+  facet_wrap(~ BG, scales = "fixed", nrow = 3, strip.position = "top") +
+  theme_classic() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major.x = element_line(color = "grey80")
+    # panel.grid.minor.x = element_line(color = "grey80"))
+  )
+
+ggsave("Psychoacoustic Change by BG.jpg",
+       plot = last_plot(), # or an explicit ggplot object name
+       path = ProjectFolder,
+       width = 1200, height = 1000, units = "px", dpi = 125)
+
+
+
+# d' for BBN --------------------------------------------------------------
+# BBN Graph (overall)
+
+TH_data %>%
+  select(ID, Sex, Condition, Type, BG_Type, BG_Intensity, `Dur (ms)`, dprime, dB) %>%
+  filter(!(ID %in% HL_not_done)) %>%
+  filter(Condition != "Recovery 2") %>%
+  # filter(Condition != "Post 2nd Exposure") %>%
+  filter(Stim == "BBN") %>%
+  # filter(Sex == "Male") %>%
+  ggplot(aes(x = dB, y = dprime)) +
+  stat_summary(aes(color = Condition, group = Condition),
+               fun = mean,
+               fun.min = function(x) mean(x) - se(x),
+               fun.max = function(x) mean(x) + se(x),
+               geom = "errorbar", width = 1, position = position_dodge(1)) +
+  stat_summary(aes(color = Condition, group = Condition),
+               fun = mean,
+               geom = "point", position = position_dodge(1), size = 3) +
+  stat_summary(aes(color = Condition, group = Condition),
+               fun = mean, geom = "line") +
+  facet_wrap(~ Type, scale = "free") +
+  theme_classic() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major.x = element_line(color = "grey80")
+    # panel.grid.minor.x = element_line(color = "grey80"))
+  )
+
+ggsave("Psychoacoustic_BBN.jpg",
+       plot = last_plot(), # or an explicit ggplot object name
+       path = ProjectFolder,
+       width = 1200, height = 600, units = "px", dpi = 100)
+
+# d' for BBN by Sex -------------------------------------------------------
+# BBN Graph (by sex)
+#
+# TH_data %>%
+#   select(ID, Sex, Condition, Type, BG_Type, BG_Intensity, `Dur (ms)`, dprime, dB) %>%
+#   filter(!(ID %in% HL_not_done)) %>%
+#   # filter(Condition != "Recovery") %>%
+#   filter(Condition != "Recovery 2") %>%
+#   # filter(Condition != "Post 2nd Exposure") %>%
+#   filter(Stim == "BBN") %>%
+#   # filter(Sex == "Male") %>%
+#   ggplot(aes(x = dB, y = dprime)) +
+#   stat_summary(aes(color = Condition, linetype = Sex, group = interaction(Condition, Sex)),
+#                fun = mean,
+#                fun.min = function(x) mean(x) - se(x),
+#                fun.max = function(x) mean(x) + se(x),
+#                geom = "errorbar", width = 1, position = position_dodge(1)) +
+#   stat_summary(aes(color = Condition, shape = Sex, group = interaction(Condition, Sex)),
+#                fun = mean,
+#                geom = "point", position = position_dodge(1), size = 3) +
+#   stat_summary(aes(color = Condition, linetype = Sex, group = interaction(Condition, Sex)),
+#                fun = mean, geom = "line") +
+#   facet_wrap(~ Type, scale = "free",) +
+#   theme_classic() +
+#   theme(
+#     plot.title = element_text(hjust = 0.5),
+#     panel.grid.major.x = element_line(color = "grey80")
+#     # panel.grid.minor.x = element_line(color = "grey80"))
+#   )
+#
+#
+# ggsave("Psychoacoustic_BBN_by_Sex.jpg",
+#        plot = last_plot(), # or an explicit ggplot object name
+#        path = ProjectFolder,
+#        width = 1200, height = 600, units = "px", dpi = 100)
+
+
+
+# d' for Tones by BG ----------------------------------------------------------
+# 4-32 Graph (Overall by BG)
+TH_data %>%
+  select(ID, Sex, Condition, Type, BG_Type, BG_Intensity, `Dur (ms)`, dprime, dB) %>%
+  filter(!(ID %in% HL_not_done)) %>%
+  # filter(Condition != "Recovery") %>%
+  filter(Condition != "Recovery 2") %>%
+  # filter(Condition != "Post 2nd Exposure") %>%
+  filter(Stim == "tone") %>%
+  # filter(Sex == "Male") %>%
+  ggplot(aes(x = dB, y = dprime)) +
+  stat_summary(aes(color = Condition, group = Condition),
+               fun = mean,
+               fun.min = function(x) mean(x) - se(x),
+               fun.max = function(x) mean(x) + se(x),
+               geom = "errorbar", width = 1, position = position_dodge(1)) +
+  stat_summary(aes(color = Condition, group = Condition),
+               fun = mean,
+               geom = "point", position = position_dodge(1), size = 3) +
+  stat_summary(aes(color = Condition, group = Condition),
+               fun = mean, geom = "line") +
+  facet_wrap(~ BG_Intensity, scale = "free", nrow = 3, strip.position = "top") +
+  theme_classic() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major.x = element_line(color = "grey80")
+    # panel.grid.minor.x = element_line(color = "grey80"))
+  )
+
+ggsave("Psychoacoustic_tone.jpg",
+       plot = last_plot(), # or an explicit ggplot object name
+       path = ProjectFolder,
+       width = 1200, height = 600, units = "px", dpi = 100)
+
+
+# d' for Tones @ 50dB PNK by kHz ------------------------------------------
+# 4-32 Graph (By Frequency & BG)
+
+TH_data %>%
+  select(ID, Sex, Condition, Type, BG_Type, BG_Intensity, `Dur (ms)`, dprime, dB) %>%
+  filter(!(ID %in% HL_not_done)) %>%
+  # filter(Condition != "Recovery") %>%
+  filter(Condition != "Recovery 2") %>%
+  filter(Condition != "Post 2nd Exposure") %>%
+  filter(Stim == "tone") %>%
+  filter(BG_Intensity == "50") %>%
+  ggplot(aes(x = dB, y = dprime)) +
+  geom_hline(yintercept = 1.5, linetype = "dashed", size = 1.2) +
+  stat_summary(aes(color = Condition, group = Condition),
+               fun = mean,
+               fun.min = function(x) mean(x) - se(x),
+               fun.max = function(x) mean(x) + se(x),
+               geom = "errorbar", width = 1, position = position_dodge(1)) +
+  stat_summary(aes(color = Condition, group = Condition),
+               fun = mean,
+               geom = "point", position = position_dodge(1), size = 3) +
+  stat_summary(aes(color = Condition, group = Condition),
+               fun = mean, geom = "line") +
+  facet_wrap(~ BG_Intensity + fct_relevel(Type, "4kHz", "8kHz", "16kHz", "32kHz"), scale = "free") +
+  xlim(20, 61) +
+  theme_classic() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major.x = element_line(color = "grey80")
+    # panel.grid.minor.x = element_line(color = "grey80"))
+  )
+
+ggsave("Psychoacoustic_tone_by_kHz.jpg",
+       plot = last_plot(), # or an explicit ggplot object name
+       path = ProjectFolder,
+       width = 2400, height = 1200, units = "px", dpi = 100)
+
+
+# d' for Tones by kHz and Sex ---------------------------------------------
+# 4-32 Graph (By Frequency and Sex)
+#
+# TH_data %>%
+#   select(ID, Sex, Condition, Type, BG_Type, BG_Intensity, `Dur (ms)`, dprime, dB) %>%
+#   filter(!(ID %in% HL_not_done)) %>%
+#   # filter(Condition != "Recovery") %>%
+#   filter(Condition != "Recovery 2") %>%
+#   filter(Condition != "Post 2nd Exposure") %>%
+#   filter(Stim == "tone") %>%
+#   # filter(Sex == "Male") %>%
+#   ggplot(aes(x = dB, y = dprime)) +
+#   stat_summary(aes(color = Condition, linetype = Sex, group = interaction(Condition, Sex)),
+#                fun = mean,
+#                fun.min = function(x) mean(x) - se(x),
+#                fun.max = function(x) mean(x) + se(x),
+#                geom = "errorbar", width = 1, position = position_dodge(1)) +
+#   stat_summary(aes(color = Condition, shape = Sex, group = interaction(Condition, Sex)),
+#                fun = mean,
+#                geom = "point", position = position_dodge(1), size = 3) +
+#   stat_summary(aes(color = Condition, linetype = Sex, group = interaction(Condition, Sex)),
+#                fun = mean, geom = "line") +
+#   facet_wrap(~ fct_relevel(Type, "4kHz", "8kHz", "16kHz", "32kHz"), scale = "free") +
+#   theme_classic() +
+#   theme(
+#     plot.title = element_text(hjust = 0.5),
+#     panel.grid.major.x = element_line(color = "grey80")
+#     # panel.grid.minor.x = element_line(color = "grey80"))
+#   )
+#
+# ggsave("Psychoacoustic_tone_by_Sex.jpg",
+#        plot = last_plot(), # or an explicit ggplot object name
+#        path = ProjectFolder,
+#        width = 2400, height = 1200, units = "px", dpi = 100)
