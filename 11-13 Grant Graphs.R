d70dadeea3b4f02b0e384264e3c144adcbae2f2a
Noelle James <github@nohelix.com>
2022-09-13 12:53:14 -0500

    Working

--- 11-13 Grant Graphs.R
+++ 11-13 Grant Graphs.R
@@ -4,7 +4,11 @@
 library(readxl); library(tidyverse); library(magrittr); library(dplyr); library(tidyr);
 
 # Data visualization
-library(ggplot2); library(forcats);
+library(ggplot2); library(forcats); library(gtools); library(ggpmisc)
+
+# Working directory -------------------------------------------------------
+ProjectFolder = "C:/Users/Noelle/Box/ABR recordings/ABR Results/Noelle/11-13kHz HHL/"
+
 
 # Select Graphing Data ----------------------------------------------------
 # Can be summarized or not
@@ -16,6 +20,12 @@ To_Graph = To_Graph %>%
                    Freq = fct_recode(as.factor(Freq), BBN = "0"),
                    Freq = fct_relevel(Freq, c("4", "8", "BBN", "16", "32")))
 
+To_Graph %>%
+  group_by(Condition, Ear, Freq) %>%
+  summarise(n = length(unique(ID)), ID = paste(unique(ID), collapse = ", ")) %>%
+  mutate(Freq = fct_relevel(Freq, c("4", "8", "16", "32", "BBN"))) %>%
+  print
+
 
 # RMS Grant Graph ---------------------------------------------------------------
 # Signal-to-Noise ratio for BBN, significant for 70-90dB
@@ -25,8 +35,8 @@ To_Graph  %>%
   stat_summary(fun = mean,
                fun.min = function(x) mean(x) - se(x),
                fun.max = function(x) mean(x) + se(x),
-               geom = "errorbar", width = 0.5) +
-  stat_summary(fun = mean, geom = "point", size = 2) +
+               geom = "errorbar", width = 3) +
+  stat_summary(fun = mean, geom = "point", size = 3) +
   stat_summary(fun = mean, geom = "line") +
   scale_x_continuous(limits = c(10, 100), breaks = c(20, 40, 60, 80, 100)) +
   labs(x = "Sound Intensity (dB)",
@@ -39,22 +49,28 @@ To_Graph  %>%
     legend.position = c(0.8, 0.2)
   )
 
-ggsave("R01_RMS.jpg",
+ggsave("11-13kHz_HHL_RMS.jpg",
        plot = last_plot(), # or an explicit ggplot object name
-       path = ProjectFolder,
-       width = 250, height = 250, units = "px", dpi = 100)
+       path = ProjectFolder)
 
 # RMS ANOVA -------------------------------------------------------------------
-RMS.aov <- aov(RMS ~ Condition * Inten * Freq, data = To_Graph)
+RMS.aov <- aov(RMS ~ Condition * Inten * Freq,
+               data = To_Graph %>%
+                        group_by(ID, Freq, Inten, Condition) %>%
+                        summarise(RMS = mean(RMS))
+               )
+
+shapiro.test(RMS.aov$residuals)$p.value
+
 summary(RMS.aov)
-TukeyHSD(RMS.aov)
-TukeyHSD(RMS.aov)$`Condition:dB` %>%
-  as_tibble(.name_repair = "unique", rownames = "Comparison") %>%
-  filter(grepl("WT:.*?-Het:.*?|Het:.*?-WT:.*?", Comparison)) %>%
-  mutate(dB1 = gsub("^.*?WT:(\\d+).*?$","\\1", Comparison),
-         dB2 = gsub("^.*?Het:(\\d+).*?$","\\1", Comparison)) %>%
-  filter(dB1 == dB2) %>%
-  mutate(sig = stars.pval(`p adj`))
+# TukeyHSD(RMS.aov)
+# TukeyHSD(RMS.aov)$`Condition:dB` %>%
+#   as_tibble(.name_repair = "unique", rownames = "Comparison") %>%
+#   filter(grepl("WT:.*?-Het:.*?|Het:.*?-WT:.*?", Comparison)) %>%
+#   mutate(dB1 = gsub("^.*?WT:(\\d+).*?$","\\1", Comparison),
+#          dB2 = gsub("^.*?Het:(\\d+).*?$","\\1", Comparison)) %>%
+#   filter(dB1 == dB2) %>%
+#   mutate(sig = stars.pval(`p adj`))
 
 
 # W1 Grant Graph ---------------------------------------------------------------
@@ -82,52 +98,67 @@ To_Graph  %>%
 
 ggsave("11-13kHz_HHL_W1lat.jpg",
        plot = last_plot(), # or an explicit ggplot object name
-       path = ProjectFolder,
-       width = 250, height = 250, units = "px", dpi = 100)
+       path = ProjectFolder)
 
 # W1 Amp ANOVA -------------------------------------------------------------------
-W1amp.aov <- aov(`W1 amp (uV)` ~ Condition * Freq * Inten, data = To_Graph)
+W1amp.aov <- aov(W1 ~ Condition * Freq * Inten,
+                 data = To_Graph %>%
+                   group_by(ID, Freq, Inten, Condition) %>%
+                   summarise(W1 = mean(`W1 amp (uV)`))
+)
 
-is_parametric = shapiro.test(Rxn.aov$residuals)$p.value > 0.05
+is_parametric = shapiro.test(W1amp.aov$residuals)$p.value > 0.05
 
 if (is_parametric == TRUE) {writeLines("Normal data proced with ANOVA")} else
-{writeLines(paste("Non-parametric data so use Kruskal followed by Dunn testing. \nShapiro Test: p =", shapiro.test(Rxn.aov$residuals)$p.value %>% round(digits = 3)))}
+{writeLines(paste("Non-parametric data so use Kruskal followed by Dunn testing. \nShapiro Test: p =", shapiro.test(W1amp.aov$residuals)$p.value %>% round(digits = 3)))}
 
 # summary(W1amp.aov)
-kruskal.test(`W1 amp (uV)` ~ interaction(Condition, Freq, Inten), data = To_Graph)
 
-pairwise.wilcox.test(To_Graph$Condition, To_Graph$Inten,
-                     p.adjust.method = "BH")
+# 80dB Graph ---------------------------------------------------------------
 
-W1amp.postHoc <-
-  FSA::dunnTest(`W1 amp (uV)` ~ interaction(Condition, Freq, Inten),
-                data = To_Graph,
-                method = "bonf")
+To_Graph_Table <-
+To_Graph %>%
+  mutate(Freq = fct_relevel(Freq, c("4", "8", "16", "32", "BBN"))) %>%
+  filter(Inten == "80") %>%
+  filter(Condition %in% c("Baseline", "1 week")) %>%
+  group_by(Freq) %>%
+  summarise(p = wilcox.test(`W1 amp (uV)` ~ Condition,
+                            exact = FALSE,
+                            alternative = "greater")$p.value %>% round(digits = 3)) %>%
+  mutate(BH = p.adjust(p, method = "BH") %>% round(digits = 3),
+         Sig = stars.pval(`BH`))
 
-W1amp.postHoc$res %>%
-  as_tibble() %>%
-  select(-P.unadj) %>%
-  mutate(Sig = gtools::stars.pval(P.adj),
-         Alone = str_split_fixed(.$Comparison, ' - ', 2)[,1],
-         Mixed = str_split_fixed(.$Comparison, ' - ', 2)[,2]) %>%
-  filter(Alone %in% c("Alone.300", "Alone.100", "Alone.50") & Mixed %in% c("Mix.300", "Mix.100", "Mix.50", "Rotating.300", "Rotating.100", "Rotating.50")) %>%
-  mutate(Alone = str_extract(Alone, "[:digit:]+") %>% as.numeric(),
-         Mixed = str_extract(Mixed, "[:digit:]+") %>% as.numeric(),
-         vs = str_extract(Comparison, "- [:alpha:]+") %>% str_remove("- "),
-         P.adj = round(P.adj, digits = 3)) %>%
-  filter(Alone == Mixed) %>%
-  select(Alone, vs, Z, P.adj, Sig) %>%
-  arrange(Alone, vs)
+# wilcox.test(`W1 amp (uV)` ~ Condition,
+#             exact = FALSE, correct = FALSE,
+#             subset = Condition %in% c("Baseline", "1 week"),
+#             data = filter(To_Graph, Inten == "80")
+# )
 
 
+To_Graph  %>%
+  filter(Inten == "80") %>%
+  mutate(Freq = fct_relevel(Freq, c("4", "8", "16", "32", "BBN"))) %>%
+  ggplot(aes(x = Condition, y = `W1 amp (uV)`, color = Freq, group = Freq)) +
+    stat_summary(fun = mean,
+                 fun.min = function(x) mean(x) - se(x),
+                 fun.max = function(x) mean(x) + se(x),
+                 geom = "errorbar", width = 0.1, position = position_dodge(0.03)) +
+    stat_summary(fun = mean, geom = "point", size = 3, position = position_dodge(0.03)) +
+    stat_summary(fun = mean, geom = "line", position = position_dodge(0.03)) +
+    labs(x = "",
+         y = expression("Wave 1 Amplitued (\u00b5V)"),
+         color = "Frequency",
+         title = "Wave 1 Amplitude at 80dB") +
+    annotate(geom = 'table',
+             x = 4.5, y = 0.1,
+             label = list(To_Graph_Table)) +
+    theme_classic() +
+    theme(
+      text = element_text(size = 12),
+      panel.grid.major.x = element_line(color = "white"),
+      legend.position = c(0.9, 0.8)
+    )
 
-# W1 Lat ANOVA ------------------------------------------------------------------
-W1lat.aov <- aov(`W1 Lat` ~ Genotype * dB, data = To_Graph)
-summary(W1lat.aov)
-TukeyHSD(W1lat.aov)$`Genotype:dB` %>%
-  as_tibble(.name_repair = "unique", rownames = "Comparison") %>%
-  filter(grepl("WT:.*?-Het:.*?|Het:.*?-WT:.*?", Comparison)) %>%
-  mutate(dB1 = gsub("^.*?WT:(\\d+).*?$","\\1", Comparison),
-         dB2 = gsub("^.*?Het:(\\d+).*?$","\\1", Comparison)) %>%
-  filter(dB1 == dB2) %>%
-  mutate(sig = stars.pval(`p adj`))
+ggsave("11-13kHz_HHL_80dB.jpg",
+       plot = last_plot(), # or an explicit ggplot object name
+       path = ProjectFolder)
