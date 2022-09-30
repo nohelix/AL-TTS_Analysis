31adf3898413c7101f85905ed24bf3b831a05d93
Noelle James <github@nohelix.com>
2022-09-30 12:29:57 -0500

    working

--- Custom Viewing.R
+++ Custom Viewing.R
@@ -28,7 +28,41 @@ TTS_Data %>%
 
 # Mixed vs. Alone BBN duration rxn ----------------------------------------
 
-Analysis_data %>%
+Data_no1st %>%
+  filter(Type == 1 & Response == "Hit") %>%
+  filter(`Inten (dB)` != -100) %>%
+  filter(Stim == "BBN" & Condition == "Baseline") %>%
+  # Filter by TH table so that only reaction times above thresholds are included
+  left_join(TTS_Data %>%
+              filter(Frequency == "BBN" & Condition == "Baseline") %>%
+              select(Date, ID, Intensity, Duration) %>%
+              rename(Range = Intensity),
+            by = c("Date", "ID")) %>%
+  group_by(ID, Sex, Condition, Stim, Duration, `Dur (ms)`, `Inten (dB)`) %>%
+  filter(ID %in% c("Green 11", "Green 12", "Orange 11", "Orange 12")) %>%
+  summarise(Rxn = mean(`R Time (ms)`)) %>%
+  mutate(Duration = fct_recode(Duration, Alone = "50ms", Alone = "100ms", Alone = "300ms", Mixed = "50-300ms"),
+         `Dur (ms)` = as.factor(`Dur (ms)`),
+         `Dur (ms)` = fct_relevel(`Dur (ms)`, c("50", "100", "300"))) %>%
+  filter(`Inten (dB)` >= 30 & `Inten (dB)` <= 80 &!(`Inten (dB)` %in% c(25, 35, 45))) %>%
+  ggplot(aes(x = `Inten (dB)`, y = Rxn, color = `Dur (ms)`, group = `Dur (ms)`)) +
+      stat_summary(fun = mean,
+                   fun.min = function(x) mean(x) - se(x),
+                   fun.max = function(x) mean(x) + se(x),
+                   geom = "errorbar", width = 3) +
+      stat_summary(fun = mean, geom = "point", size = 3) +
+      stat_summary(fun = mean, geom = "line") +
+      # scale_x_continuous(limits = c(28, 92), breaks = c(30, 50, 70, 90)) +
+      labs(x = "Sound Intensity (dB)",
+           y = "Reaction time (ms)") +
+      facet_wrap(~ Duration, nrow = 2) +
+      theme_classic() +
+      theme(
+        text = element_text(size = 12),
+        panel.grid.major.x = element_line(color = "white")
+      )
+
+Test.df = Data_no1st %>%
   filter(Type == 1 & Response == "Hit") %>%
   filter(`Inten (dB)` != -100) %>%
   filter(Stim == "BBN" & Condition == "Baseline") %>%
@@ -40,26 +74,29 @@ Analysis_data %>%
             by = c("Date", "ID")) %>%
   group_by(ID, Sex, Condition, Stim, Duration, `Dur (ms)`, `Inten (dB)`) %>%
   summarise(Rxn = mean(`R Time (ms)`)) %>%
-  mutate(Duration = fct_recode(Duration, Alone = "50ms", Alone = "300ms", Mixed = "50-300ms"),
-         `Dur (ms)` = fct_recode(as.factor(`Dur (ms)`), "50ms" = "50", "100ms" = "100", "300ms" = "300"),
-         `Dur (ms)` = fct_relevel(`Dur (ms)`, c("50ms", "100ms", "300ms"))) %>%
-  filter(`Inten (dB)` >= 30) %>%
-  ggplot(aes(x = `Inten (dB)`, y = Rxn, color = `Dur (ms)`, group = `Dur (ms)`)) +
-      stat_summary(fun = mean,
-                   fun.min = function(x) mean(x) - se(x),
-                   fun.max = function(x) mean(x) + se(x),
-                   geom = "errorbar", width = 3) +
-      stat_summary(fun = mean, geom = "point", size = 3) +
-      stat_summary(fun = mean, geom = "line") +
-      scale_x_continuous(limits = c(28, 92), breaks = c(30, 50, 70, 90)) +
-      labs(x = "Sound Intensity (dB)",
-           y = "Reaction time (ms)") +
-      facet_wrap(~ Duration) +
-      theme_classic() +
-      theme(
-        text = element_text(size = 12),
-        panel.grid.major.x = element_line(color = "white")
-      )
+  mutate(Duration = fct_recode(Duration, Alone = "50ms", Alone = "100ms", Alone = "300ms", Mixed = "50-300ms"),
+         `Dur (ms)` = as.factor(`Dur (ms)`),
+         `Dur (ms)` = fct_relevel(`Dur (ms)`, c("50", "100", "300"))) %>%
+  filter(`Inten (dB)` >= 30 & `Inten (dB)` <= 80 &!(`Inten (dB)` %in% c(25, 35, 45))) %>%
+  filter(ID %in% c("Orange 11", "Orange 12", "Green 11", "Green 12") & Duration == "Alone") %>%
+  rename(Intensity = `Inten (dB)`,
+         Dur = `Dur (ms)`)
+
+Test.aov = aov(LambertW::Gaussianize(Test.df$Rxn) ~ Intensity * Dur, data = Test.df)
+shapiro.test(Test.aov$residuals)
+
+summary(Test.aov)
+
+TukeyHSD(Test.aov, "Dur") %>% tidy() %>% mutate(sig = gtools::stars.pval(adj.p.value))
+
+# Missing raw files -------------------------------------------------------
+File_list_verify %>%
+  filter(is.na(FileName_complete)) %>%
+  # .mat files from 9-22-2021 and earlier are in the old format and can not be exported currently
+  filter(Date > "2021-9-22") %>%
+  filter(Date < Sys.Date()) %>%
+  arrange(Date) %>%
+  View
 
 # Threshold filtering -----------------------------------------------------
 
